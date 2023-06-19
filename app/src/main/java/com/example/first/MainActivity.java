9da9a346ba570309d2e383043797eddf055d5522
Jishnuraj P <jrpjishnuraj@gmail.com>
2023-06-19 17:53:47 -0400

    Successfully added textinputlayout and made changes to buttons.

--- app/src/main/java/com/example/first/MainActivity.java
+++ app/src/main/java/com/example/first/MainActivity.java
@@ -23,74 +23,74 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void Btnclick(View v) throws SQLException, ClassNotFoundException {
-
-        getvalue_database(v);
-    }
-    public void getvalue_database(View view) throws SQLException, ClassNotFoundException {
-
-        Thread t= new Thread(new Runnable() {
-            TextView t1,t2 ;
-
-            @Override
-            public void run() {
-                t1=findViewById(R.id.user_name);
-                t2=findViewById(R.id.Pass);
-                EditText e1;
-                Connect_SQL connectSql = new Connect_SQL("JP", "jrpjp#321",
-                        "129.21.136.123", "first", "3306");
-                try {
-                    connection = connectSql.Connection_get();
-                } catch (ClassNotFoundException e) {
-                    throw new RuntimeException(e);
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-
-                String query = "select * from samplespace1 where Username='JP';";
-                Statement s1 = null;
-                try {
-                    s1 = connection.createStatement();
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-                ResultSet res = null;
-                try {
-                    res = s1.executeQuery(query);
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-
-                ResultSet finalRes = res;
-                runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            while (true) {
-                                try {
-                                    if (!finalRes.next()) break;
-                                } catch (SQLException e) {
-                                    throw new RuntimeException(e);
-                                }
-                            try {
-                                t1.setText(finalRes.getString(2));
-                            } catch (SQLException e) {
-                                throw new RuntimeException(e);
-                            }
-
-                            try {
-                                t2.setText(finalRes.getString(4));
-                            } catch (SQLException e) {
-                                throw new RuntimeException(e);
-                            }
-                        }
-                        }
-                    });
-
-
-
-            }
-        });
-        t.start();
-
-    }
+//    public void Btnclick(View v) throws SQLException, ClassNotFoundException {
+//
+//        getvalue_database(v);
+//    }
+//    public void getvalue_database(View view) throws SQLException, ClassNotFoundException {
+//
+//        Thread t= new Thread(new Runnable() {
+//            TextView t1,t2 ;
+//
+//            @Override
+//            public void run() {
+//                t1=findViewById(R.id.user_name);
+//                t2=findViewById(R.id.Pass);
+//                EditText e1;
+//                Connect_SQL connectSql = new Connect_SQL("JP", "jrpjp#321",
+//                        "129.21.136.123", "first", "3306");
+//                try {
+//                    connection = connectSql.Connection_get();
+//                } catch (ClassNotFoundException e) {
+//                    throw new RuntimeException(e);
+//                } catch (SQLException e) {
+//                    throw new RuntimeException(e);
+//                }
+//
+//                String query = "select * from samplespace1 where Username='JP';";
+//                Statement s1 = null;
+//                try {
+//                    s1 = connection.createStatement();
+//                } catch (SQLException e) {
+//                    throw new RuntimeException(e);
+//                }
+//                ResultSet res = null;
+//                try {
+//                    res = s1.executeQuery(query);
+//                } catch (SQLException e) {
+//                    throw new RuntimeException(e);
+//                }
+//
+//                ResultSet finalRes = res;
+//                runOnUiThread(new Runnable() {
+//                        @Override
+//                        public void run() {
+//                            while (true) {
+//                                try {
+//                                    if (!finalRes.next()) break;
+//                                } catch (SQLException e) {
+//                                    throw new RuntimeException(e);
+//                                }
+//                            try {
+//                                t1.setText(finalRes.getString(2));
+//                            } catch (SQLException e) {
+//                                throw new RuntimeException(e);
+//                            }
+//
+//                            try {
+//                                t2.setText(finalRes.getString(4));
+//                            } catch (SQLException e) {
+//                                throw new RuntimeException(e);
+//                            }
+//                        }
+//                        }
+//                    });
+//
+//
+//
+//            }
+//        });
+//        t.start();
+//
+//    }
 }
