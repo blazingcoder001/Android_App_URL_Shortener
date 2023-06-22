525f94ed85436b0193211ad6152eb1916aebe7dc
Jishnuraj P <jrpjishnuraj@gmail.com>
2023-06-22 12:19:02 -0400

    Added drawables for eye and eye_off for showing and hiding password. Succesffully retrieved account information from login.

--- app/src/main/java/com/example/first/MainActivity.java
+++ app/src/main/java/com/example/first/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.first;
 
-import androidx.annotation.MainThread;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -17,80 +24,122 @@ public class MainActivity extends AppCompatActivity {
 
     Connection connection;
 
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextInputLayout userl=findViewById(R.id.username);
+        EditText user=userl.getEditText();
+        TextInputLayout passwordl=findViewById(R.id.password);
+        EditText password=passwordl.getEditText();
+        ViewGroup.LayoutParams from= password.getLayoutParams();
+        user.setLayoutParams(from);
+        password.setOnTouchListener(new View.OnTouchListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                final int DRAWABLE_RIGHT =2;
+                if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    if (motionEvent.getRawX() >= (password.getRight() - password.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                        // Toggle password visibility
+                        if (password.getTransformationMethod() == PasswordTransformationMethod.getInstance()) {
+                            password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                                password.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.eye_off, 0);
+                            }
+                        } else {
+                            password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                                password.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.eye, 0);
+                            }
+                        }
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+
+        });
     }
 
-//    public void Btnclick(View v) throws SQLException, ClassNotFoundException {
-//
-//        getvalue_database(v);
-//    }
-//    public void getvalue_database(View view) throws SQLException, ClassNotFoundException {
-//
-//        Thread t= new Thread(new Runnable() {
-//            TextView t1,t2 ;
-//
-//            @Override
-//            public void run() {
-//                t1=findViewById(R.id.user_name);
-//                t2=findViewById(R.id.Pass);
-//                EditText e1;
-//                Connect_SQL connectSql = new Connect_SQL("JP", "jrpjp#321",
-//                        "129.21.136.123", "first", "3306");
-//                try {
-//                    connection = connectSql.Connection_get();
-//                } catch (ClassNotFoundException e) {
-//                    throw new RuntimeException(e);
-//                } catch (SQLException e) {
-//                    throw new RuntimeException(e);
-//                }
-//
-//                String query = "select * from samplespace1 where Username='JP';";
-//                Statement s1 = null;
-//                try {
-//                    s1 = connection.createStatement();
-//                } catch (SQLException e) {
-//                    throw new RuntimeException(e);
-//                }
-//                ResultSet res = null;
-//                try {
-//                    res = s1.executeQuery(query);
-//                } catch (SQLException e) {
-//                    throw new RuntimeException(e);
-//                }
-//
-//                ResultSet finalRes = res;
-//                runOnUiThread(new Runnable() {
-//                        @Override
-//                        public void run() {
-//                            while (true) {
-//                                try {
-//                                    if (!finalRes.next()) break;
-//                                } catch (SQLException e) {
-//                                    throw new RuntimeException(e);
-//                                }
-//                            try {
-//                                t1.setText(finalRes.getString(2));
-//                            } catch (SQLException e) {
-//                                throw new RuntimeException(e);
-//                            }
-//
-//                            try {
-//                                t2.setText(finalRes.getString(4));
-//                            } catch (SQLException e) {
-//                                throw new RuntimeException(e);
-//                            }
-//                        }
-//                        }
-//                    });
-//
-//
-//
-//            }
-//        });
-//        t.start();
-//
-//    }
+
+
+
+    public void Btnclick(View v)  {
+
+        getvalue_database(v);
+    }
+    public void getvalue_database(View view)  {
+
+        Thread t= new Thread(new Runnable() {
+            TextView t1 ;
+            TextInputLayout userl,passwordl;
+            EditText user,password;
+
+
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public void run() {
+
+                Connect_SQL connectSql = new Connect_SQL("JP", "jrpjp#321",
+                        "129.21.136.123", "first", "3306");
+                try {
+                    connection = connectSql.Connection_get();
+                } catch (ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+                userl=findViewById(R.id.username);
+                user=userl.getEditText();
+                passwordl=findViewById(R.id.password);
+                password=passwordl.getEditText();
+                t1=findViewById(R.id.not_user);
+
+
+                String query = "select * from samplespace1 where Username='"+user.getText().toString()+"' and Password='"+password.getText().toString()+"';";
+                Statement s1 = null;
+                try {
+                    s1 = connection.createStatement();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+                ResultSet res = null;
+                try {
+                    res = s1.executeQuery(query);
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+
+                ResultSet finalRes = res;
+                runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            while (true) {
+                                try {
+                                    if (!finalRes.next()) break;
+                                } catch (SQLException e) {
+                                    throw new RuntimeException(e);
+                                }
+                            try {
+                                t1.setText(finalRes.getString(2));
+                            } catch (SQLException e) {
+                                throw new RuntimeException(e);
+                            }
+
+
+                        }
+                        }
+                    });
+
+
+
+            }
+        });
+        t.start();
+
+    }
 }
