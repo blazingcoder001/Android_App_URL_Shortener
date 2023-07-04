13c3928f335da8810f4ea1f83cccfab7d63fb8ad
Jishnuraj P <jrpjishnuraj@gmail.com>
2023-07-04 16:14:12 -0400

    Implementing functionality of Navigation drawer. There are some bugs regarding the priority of active layers.

--- app/src/main/java/com/example/first/SignIn.java
+++ app/src/main/java/com/example/first/SignIn.java
@@ -1,11 +1,16 @@
 package com.example.first;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class SignIn extends AppCompatActivity {
     @Override
@@ -14,6 +19,21 @@ public class SignIn extends AppCompatActivity {
         setContentView(R.layout.sign_in);
         DrawerLayout side=findViewById(R.id.drawer);
         side.setScrimColor(ContextCompat.getColor(this,R.color.scrim));
+        side.closeDrawer(GravityCompat.START);
+        MaterialToolbar topappbar= findViewById(R.id.topAppBar);
+        topappbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!side.isDrawerOpen(GravityCompat.START)) {
+                    side.openDrawer(GravityCompat.START);
+                    topappbar.setTitle("Settings");
+                }
+                else{
+                    side.closeDrawer(GravityCompat.START);
+                    topappbar.setTitle(null);
+                }
+            }
+        });
 
     }
 }
